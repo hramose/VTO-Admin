5d791bba47185a7c35e57c36e3cb8f53f8e30b71
Bernd Obendorfer <bernd.obendorfer@chello.at>
2015-10-04 14:54:09 +0200

    favicion

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -18,6 +18,7 @@
           <title>VTO - Admin</title>
           <meta name="description" content="">
           <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> 
 
            <!-- jQuery 2.1.4 -->
             <!--<script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>-->
--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -5,6 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> 
 
 
 	<head>
