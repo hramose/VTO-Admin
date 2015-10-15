449dabb3410b8cb402728e244d351135dd6734a3
Bernd Obendorfer <bernd.obendorfer@chello.at>
2015-10-15 22:12:11 +0200

    k

--- resources/views/back/tasklist/index.blade.php
+++ resources/views/back/tasklist/index.blade.php
@@ -139,6 +139,192 @@
 <!-- page script --> 
 <!-- DataTables script and style-->
 </div>
+
+
+
+
+
+
+
+
+
+
+<table class="tablesorter">
+    <thead>
+        <tr>
+            <th>AlphaNumeric</th>
+            <th>Numeric</th>
+            <th>Animals</th>
+            <th>Sites</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr><td>abc 123</td><td>10</td><td>Koala</td><td>http://www.google.com</td></tr>
+        <tr><td>abc 1</td><td>234</td><td>Ox</td><td>http://www.yahoo.com</td></tr>
+        <tr><td>abc 9</td><td>10</td><td>Girafee</td><td>http://www.facebook.com</td></tr>
+        <tr><td>zyx 24</td><td>767</td><td>Bison</td><td>http://www.whitehouse.gov/</td></tr>
+        <tr><td>abc 11</td><td>3</td><td>Chimp</td><td>http://www.ucla.edu/</td></tr>
+        <tr><td>abc 2</td><td>56</td><td>Elephant</td><td>http://www.wikipedia.org/</td></tr>
+        <tr><td>abc 9</td><td>155</td><td>Lion</td><td>http://www.nytimes.com/</td></tr>
+        <tr><td>ABC 10</td><td>87</td><td>Zebra</td><td>http://www.google.com</td></tr>
+        <tr><td>zyx 1</td><td>999</td><td>Koala</td><td>http://www.mit.edu/</td></tr>
+        <tr><td>zyx 12</td><td>0</td><td>Llama</td><td>http://www.nasa.gov/</td></tr>
+    </tbody>
+</table>
+
+
+
+   <script src="http://mottie.github.io/tablesorter/js/jquery.tablesorter.js"></script>
+
+   <script src="http://mottie.github.io/tablesorter/js/jquery.tablesorter.widgets.js"></script>
+
+
+<script type="text/javascript">
+   
+  $('table')
+    .bind('filterInit', function() {
+        // check that storage ulility is loaded
+        if ($.tablesorter.storage) {
+            // get saved filters
+            var f = $.tablesorter.storage(this, 'tablesorter-filters') || [];
+            $(this).trigger('search', [f]);
+        }
+    })
+    .bind('filterEnd', function(){
+        if ($.tablesorter.storage) {
+            // save current filters
+            var f = $(this).find('.tablesorter-filter').map(function(){
+                return $(this).val() || '';
+            }).get();
+            $.tablesorter.storage(this, 'tablesorter-filters', f);
+        }
+    });
+
+
+$('table').tablesorter({
+
+    // *** APPEARANCE ***
+    // Add a theme - try 'blackice', 'blue', 'dark', 'default'
+    theme: 'blue',
+
+    // fix the column widths
+    widthFixed: true,
+
+    // include zebra and any other widgets, options:
+    // 'columns', 'filter', 'stickyHeaders' & 'resizable'
+    // 'uitheme' is another widget, but requires loading
+    // a different skin and a jQuery UI theme.
+    widgets: ['zebra', 'filter'],
+
+    widgetOptions: {
+
+        // zebra widget: adding zebra striping, using content and
+        // default styles - the ui css removes the background
+        // from default even and odd class names included for this
+        // demo to allow switching themes
+        // [ "even", "odd" ]
+        zebra: [
+            "ui-widget-content even",
+            "ui-state-default odd"
+            ],
+
+        // uitheme widget: * Updated! in tablesorter v2.4 **
+        // Instead of the array of icon class names, this option now
+        // contains the name of the theme. Currently jQuery UI ("jui")
+        // and Bootstrap ("bootstrap") themes are supported. To modify
+        // the class names used, extend from the themes variable
+        // look for the "$.extend($.tablesorter.themes.jui" code below
+        uitheme: 'jui',
+
+        // columns widget: change the default column class names
+        // primary is the 1st column sorted, secondary is the 2nd, etc
+        columns: [
+            "primary",
+            "secondary",
+            "tertiary"
+            ],
+
+        // columns widget: If true, the class names from the columns
+        // option will also be added to the table tfoot.
+        columns_tfoot: true,
+
+        // columns widget: If true, the class names from the columns
+        // option will also be added to the table thead.
+        columns_thead: true,
+
+        // filter widget: If there are child rows in the table (rows with
+        // class name from "cssChildRow" option) and this option is true
+        // and a match is found anywhere in the child row, then it will make
+        // that row visible; default is false
+        filter_childRows: false,
+
+        // filter widget: If true, a filter will be added to the top of
+        // each table column.
+        filter_columnFilters: true,
+
+        // filter widget: css class applied to the table row containing the
+        // filters & the inputs within that row
+        filter_cssFilter: "tablesorter-filter",
+
+        // filter widget: Customize the filter widget by adding a select
+        // dropdown with content, custom options or custom filter functions
+        // see http://goo.gl/HQQLW for more details
+        filter_functions: null,
+
+        // filter widget: Set this option to true to hide the filter row
+        // initially. The rows is revealed by hovering over the filter
+        // row or giving any filter input/select focus.
+        filter_hideFilters: false,
+
+        // filter widget: Set this option to false to keep the searches
+        // case sensitive
+        filter_ignoreCase: true,
+
+        // filter widget: jQuery selector string of an element used to
+        // reset the filters. 
+        filter_reset: null,
+
+        // Delay in milliseconds before the filter widget starts searching;
+        // This option prevents searching for every character while typing
+        // and should make searching large tables faster.
+        filter_searchDelay: 300,
+
+        // filter widget: Set this option to true to use the filter to find
+        // text from the start of the column. So typing in "a" will find
+        // "albert" but not "frank", both have a's; default is false
+        filter_startsWith: false,
+
+        // filter widget: If true, ALL filter searches will only use parsed
+        // data. To only use parsed data in specific columns, set this option
+        // to false and add class name "filter-parsed" to the header
+        filter_useParsedData: false,
+
+        // Resizable widget: If this option is set to false, resized column
+        // widths will not be saved. Previous saved values will be restored
+        // on page reload
+        resizable: true,
+
+        // saveSort widget: If this option is set to false, new sorts will
+        // not be saved. Any previous saved sort will be restored on page
+        // reload.
+        saveSort: true,
+
+        // stickyHeaders widget: css class name applied to the sticky header
+        stickyHeaders: "tablesorter-stickyHeader"
+
+    }
+
+});
+</script>
+
+
+
+
+
+
+
+
+
 @else
 @include('back.partials.secure') 
 </div>
